c2b70491d40768598968e063c31f30c97e081795
yosshii50 <kasaubunts@gmail.com>
2020-11-30 15:15:12 +0900

    first commit]

---
+++ WinPortListCnv/WinPortListCnv/Module1.vb
@@ -0,0 +1,172 @@
+﻿
+'実行サンプル1
+'   cscript cscript C:\Windows\System32\Printing_Admin_Scripts\ja-JP\prnport.vbs -l | D:\GitHub\WinPortListCnv\WinPortListCnv\bin\Debug\WinPortListCnv.exe | clip
+
+'実行サンプル2
+'   cscript C:\Windows\System32\Printing_Admin_Scripts\ja-JP\prnport.vbs -l > ポート一覧.txt
+'   WinPortListCnv.exe < ポート一覧.txt | clip
+
+Module Module1
+
+    Structure PortInfo_Str
+        Dim PortName As String
+        Dim Address As String
+        Dim Protocol As String
+        Dim PortNo As String
+        Dim Queue As String
+        Dim ByteCount As String
+        Dim SNMP As String
+        Dim Community As String
+        Dim DeviceIndex As String
+    End Structure
+
+    Sub Main()
+
+        Dim PortInfoList = New List(Of PortInfo_Str)()
+
+        Dim WrkPortInfo As New PortInfo_Str
+        While True
+            Dim WrkLine As String
+            Dim WrkStr As String
+
+            WrkLine = Console.ReadLine()
+            If WrkLine Is Nothing Then
+                If WrkPortInfo.PortName <> "" Then
+                    PortInfoList.Add(WrkPortInfo)
+                End If
+                Exit While
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "ポート名 ")
+            If WrkStr <> "" Then
+                If WrkPortInfo.PortName <> "" Then
+                    PortInfoList.Add(WrkPortInfo)
+                End If
+                WrkPortInfo.PortName = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "ホスト アドレス ")
+            If WrkStr <> "" Then
+                WrkPortInfo.Address = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "プロトコル ")
+            If WrkStr <> "" Then
+                WrkPortInfo.Protocol = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "ポート番号 ")
+            If WrkStr <> "" Then
+                WrkPortInfo.PortNo = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "キュー ")
+            If WrkStr <> "" Then
+                WrkPortInfo.Queue = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "バイト カウント")
+            If WrkStr <> "" Then
+                WrkPortInfo.ByteCount = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "SNMP ")
+            If WrkStr <> "" Then
+                WrkPortInfo.SNMP = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "コミュニティ ")
+            If WrkStr <> "" Then
+                WrkPortInfo.Community = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "デバイス インデックス ")
+            If WrkStr <> "" Then
+                WrkPortInfo.DeviceIndex = WrkStr
+            End If
+
+        End While
+
+        If 1 = 0 Then
+
+            '一覧表出力
+            Dim FirstFLG As Boolean = True
+            For Each EachPortInfo In PortInfoList
+                If FirstFLG = True Then
+                    Console.Write("ポート名" & vbTab)
+                    Console.Write("ホストアドレス" & vbTab)
+                    Console.Write("プロトコル" & vbTab)
+                    Console.Write("ポート番号" & vbTab)
+                    Console.Write("キュー" & vbTab)
+                    Console.Write("バイトカウント" & vbTab)
+                    Console.Write("SNMP" & vbTab)
+                    Console.Write("コミュニティ" & vbTab)
+                    Console.Write("デバイスインデックス" & vbCrLf)
+                    FirstFLG = False
+                End If
+                Console.Write(EachPortInfo.PortName & vbTab)
+                Console.Write(EachPortInfo.Address & vbTab)
+                Console.Write(EachPortInfo.Protocol & vbTab)
+                Console.Write(EachPortInfo.PortNo & vbTab)
+                Console.Write(EachPortInfo.Queue & vbTab)
+                Console.Write(EachPortInfo.ByteCount & vbTab)
+                Console.Write(EachPortInfo.SNMP & vbTab)
+                Console.Write(EachPortInfo.Community & vbTab)
+                Console.Write(EachPortInfo.DeviceIndex & vbCrLf)
+            Next
+
+        Else
+
+            'Port生成用コマンド
+            Console.WriteLine("管理者として cmd 実行")
+            Console.WriteLine("cd C:\Windows\System32\Printing_Admin_Scripts\ja-JP")
+            For Each EachPortInfo In PortInfoList
+                Console.Write("cscript prnport.vbs -a -r " & EachPortInfo.PortName)
+                Console.Write(" -h " & EachPortInfo.Address)
+                If EachPortInfo.Protocol = "RAW" Then
+                    'RAW
+                    Console.Write(" -o raw -n " & EachPortInfo.PortNo)
+                Else
+                    'LPR
+                    Console.Write(" -o lpr -q " & EachPortInfo.Queue)
+                    If EachPortInfo.ByteCount = "有効" Then
+                        '有効
+                        Console.Write(" -2e")
+                    Else
+                        '無効
+                        Console.Write(" -2d")
+                    End If
+                End If
+                If EachPortInfo.SNMP = "無効" Then
+                    '無効
+                    Console.Write(" -md")
+                Else
+                    '有効
+                    Console.Write(" -me -y " & EachPortInfo.Community & " -i " & EachPortInfo.DeviceIndex)
+                End If
+                Console.Write(vbCrLf)
+            Next
+
+        End If
+
+        ''デバッグの場合、画面が消えるので対策
+        'Console.WriteLine(vbCrLf + vbCrLf + "続行するには Ctrl+C を押してください．．．")
+        'While True
+        '    Console.Write("")
+        'End While
+
+    End Sub
+
+    Private Function GetInfoMsg(ByVal BaseStr As String, ByVal CheckStr As String) As String
+
+        Dim StrPos = BaseStr.IndexOf(CheckStr)
+
+        If StrPos >= 0 Then
+            StrPos = StrPos + CheckStr.Length
+            Return BaseStr.Substring(StrPos)
+        End If
+
+        Return ""
+    End Function
+
+End Module
---
+++ WinPrinterListCnv/WinPrinterListCnv/Module1.vb
@@ -0,0 +1,112 @@
+﻿
+'実行サンプル1
+'   cscript C:\Windows\System32\Printing_Admin_Scripts\ja-JP\prnmngr.vbs -l | D:\GitHub\WinPrinterListCnv\WinPrinterListCnv\bin\Debug\WinPrinterListCnv.exe | clip
+
+'実行サンプル2
+'   cscript C:\Windows\System32\Printing_Admin_Scripts\ja-JP\prnmngr.vbs -l > プリンタ一覧.txt
+'   WinPrinterListCnv.exe < プリンタ一覧.txt | clip
+
+Module Module1
+
+    Structure PrintInfo_Str
+        Dim PrinterName As String
+        Dim ShareName As String
+        Dim DriverName As String
+        Dim PortName As String
+        Dim Comment As String
+        Dim Location As String
+    End Structure
+
+    Sub Main()
+
+        Dim PrintInfoList = New List(Of PrintInfo_Str)()
+
+        Dim WrkPrintInfo As New PrintInfo_Str
+        While True
+            Dim WrkLine As String
+            Dim WrkStr As String
+
+            WrkLine = Console.ReadLine()
+            If WrkLine Is Nothing Then
+                If WrkPrintInfo.PrinterName <> "" Then
+                    PrintInfoList.Add(WrkPrintInfo)
+                End If
+                Exit While
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "プリンター名 ")
+            If WrkStr <> "" Then
+                If WrkPrintInfo.PrinterName <> "" Then
+                    PrintInfoList.Add(WrkPrintInfo)
+                End If
+                WrkPrintInfo.PrinterName = WrkStr
+                'Console.WriteLine(WrkStr)
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "共有名 ")
+            If WrkStr <> "" Then
+                WrkPrintInfo.ShareName = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "ドライバー名 ")
+            If WrkStr <> "" Then
+                WrkPrintInfo.DriverName = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "ポート名 ")
+            If WrkStr <> "" Then
+                WrkPrintInfo.PortName = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "コメント ")
+            If WrkStr <> "" Then
+                WrkPrintInfo.Comment = WrkStr
+            End If
+
+            WrkStr = GetInfoMsg(WrkLine, "場所 ")
+            If WrkStr <> "" Then
+                WrkPrintInfo.Location = WrkStr
+            End If
+
+        End While
+
+        Dim FirstFLG As Boolean = True
+        For Each EachPrintInfo In PrintInfoList
+            If FirstFLG = True Then
+                Console.Write("プリンター名" & vbTab)
+                Console.Write("共有名" & vbTab)
+                Console.Write("ドライバー名" & vbTab)
+                Console.Write("ポート名" & vbTab)
+                Console.Write("コメント" & vbTab)
+                Console.Write("場所" & vbCrLf)
+                FirstFLG = False
+            End If
+            Console.Write(EachPrintInfo.PrinterName & vbTab)
+            Console.Write(EachPrintInfo.ShareName & vbTab)
+            Console.Write(EachPrintInfo.DriverName & vbTab)
+            Console.Write(EachPrintInfo.PortName & vbTab)
+            Console.Write(EachPrintInfo.Comment & vbTab)
+            Console.Write(EachPrintInfo.Location & vbCrLf)
+        Next
+
+        ''デバッグの場合、画面が消えるので対策
+        'Console.WriteLine(vbCrLf + vbCrLf + "続行するには Ctrl+C を押してください．．．")
+        'While True
+        '    Console.Write("")
+        'End While
+
+    End Sub
+
+    Private Function GetInfoMsg(ByVal BaseStr As String, ByVal CheckStr As String) As String
+
+        Dim StrPos = BaseStr.IndexOf(CheckStr)
+
+        If StrPos >= 0 Then
+            StrPos = StrPos + CheckStr.Length
+            Return BaseStr.Substring(StrPos)
+        End If
+
+        Return ""
+    End Function
+
+End Module
